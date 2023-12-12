499a716fdae99078944d79776429f14bf51ca940
sebastiaobnj <junin_sbn@hotmail.com>
2023-12-12 12:48:22 -0300

    corrigindo lint 12-48

--- backend/tests/mocks/sales.mock.js
+++ backend/tests/mocks/sales.mock.js
@@ -1,60 +1,76 @@
 const newSaleMock = [
-    {
-      productId: 1,
-      quantity: 1,
-    },
-    {
-      productId: 2,
-      quantity: 5,
-    },
-  ];
+  {
+    productId: 1,
+    quantity: 1,
+  },
+  {
+    productId: 2,
+    quantity: 5,
+  },
+];
   
-  const newSaleMockIdError = [
-    {
-      productId: 123,
-      quantity: 1,
-    },
-    {
-      productId: 2,
-      quantity: 5,
-    },
-  ];
+const newSaleMockIdError = [
+  {
+    productId: 123,
+    quantity: 1,
+  },
+  {
+    productId: 2,
+    quantity: 5,
+  },
+];
   
-  const newSaleNoIdMock = [
-    {
-      quantity: 1,
-    },
-    {
-      productId: 2,
-      quantity: 5,
-    },
-  ];
+const newSaleNoIdMock = [
+  {
+    quantity: 1,
+  },
+  {
+    productId: 2,
+    quantity: 5,
+  },
+];
   
-  const newSaleMockQuantityError = [
-    {
-      productId: 1,
-    },
-    {
-      productId: 2,
-      quantity: 5,
-    },
-  ];
+const newSaleMockQuantityError = [
+  {
+    productId: 1,
+  },
+  {
+    productId: 2,
+    quantity: 5,
+  },
+];
   
-  const newSaleMockQuantityZeroError = [
-    {
-      productId: 1,
-      quantity: 0,
-    },
-    {
-      productId: 2,
-      quantity: 5,
-    },
-  ];
+const newSaleMockQuantityZeroError = [
+  {
+    productId: 1,
+    quantity: 0,
+  },
+  {
+    productId: 2,
+    quantity: 5,
+  },
+];
   
-  const newSaleReturnMock = {
+const newSaleReturnMock = {
+  status: 201,
+  data: {
+    id: 6,
+    itemsSold: [
+    {
+        productId: 1,
+        quantity: 1,
+    },
+    {
+        productId: 2,
+        quantity: 5,
+    },
+    ],
+  },
+};
+  
+const insertSaleMock = {
     status: 201,
     data: {
-      id: 6,
       itemsSold: [
         {
           productId: 1,
@@ -66,25 +82,9 @@ const newSaleMock = [
         },
       ],
     },
-  };
+};
   
-  const insertSaleMock = {
-    status: 201,
-    data: {
-      itemsSold: [
-        {
-          productId: 1,
-          quantity: 1,
-        },
-        {
-          productId: 2,
-          quantity: 5,
-        },
-      ],
-    },
-  };
-  
-  const updateSaleQuantityMock = {
+const updateSaleQuantityMock = {
     status: 200,
     data: {
       date: '2023-11-25T20:57:05.000Z',
@@ -92,9 +92,9 @@ const newSaleMock = [
       quantity: 20,
       saleId: 1,
     },
-  };
+};
   
-  const errorSale = [
+const errorSale = [
     {
       date: '2023-11-25T20:57:05.000Z',
       productId: 1,
@@ -105,11 +105,11 @@ const newSaleMock = [
       productId: 2,
       quantity: 20,
     },
-  ];
+];
   
-  const errorProduct = [{ id: 2, name: 'Traje de encolhimento' }];
+const errorProduct = [{ id: 2, name: 'Traje de encolhimento' }];
   
-  module.exports = {
+module.exports = {
     newSaleMock,
     newSaleReturnMock,
     insertSaleMock,
@@ -120,4 +120,4 @@ const newSaleMock = [
     updateSaleQuantityMock,
     errorSale,
     errorProduct,
-  };
+};
