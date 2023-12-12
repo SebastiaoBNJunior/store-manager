fdedc746f61984e47c186947aa2fb0d9fdd2f76a
sebastiaobnj <junin_sbn@hotmail.com>
2023-12-12 12:37:48 -0300

    edit code 11

--- backend/tests/unit/models/products.model.test.js
+++ backend/tests/unit/models/products.model.test.js
@@ -24,6 +24,43 @@ describe('Realizando testes para listagem dos produtos', function () {
     expect(findProductsById).to.be.deep.equal([producByIdModel]);
   });
 
+  it('Cadastrando produtos no model com sucesso', async function () {
+    const modelResolve = { id: 3, name: 'ProdutoX' };
+    sinon.stub(connection, 'execute').onFirstCall().resolves([{ insertId: 3 }])
+      .onSecondCall()
+      .resolves([[modelResolve]]);
+
+    const newProduct = await productsModel.insertProductsModel('ProdutoX');
+
+    expect(newProduct).to.be.deep.equal(modelResolve);
+  });
+
+  it('Atualizando produtos no model com sucesso', async function () {
+    sinon.stub(connection, 'execute').resolves([]);
+
+    const newProduct = await productsModel.updateProductModel(1, 'Martelo do Batman');
+
+    expect(newProduct).to.be.deep.equal({ status: 200, data: { id: 1, name: 'Martelo do Batman' } });
+  });
+
+  it('Deletando produtos no model com sucesso', async function () {
+    sinon.stub(connection, 'execute').resolves([]);
+
+    const newProduct = await productsModel.deleteProductModel(1);
+
+    expect(newProduct).to.be.deep.equal({ status: 204 });
+  });
+
+  it('Pesquisando produtos no model com sucesso', async function () {
+    const findAllModelReturn = [{ id: 1, name: 'Martelo de Thor' }];
+    sinon.stub(connection, 'execute').resolves([findAllModelReturn]);
+
+    const result = await productsModel.searchProductModel('Martelo');
+
+    expect(result.status).to.be.equal(200);
+    expect(result.data).to.be.deep.equal(findAllModelReturn);
+  });
+
   afterEach(function () {
     sinon.restore();
   });
--- backend/tests/unit/services/products.service.test.js
+++ backend/tests/unit/services/products.service.test.js
@@ -1,16 +1,16 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 // const connection = require('../../../src/models/connection');
-const { products } = require('../../../src/services');
+const { productsService } = require('../../../src/services');
 const { productsModel } = require('../../../src/models');
-const productsController = require('../../../src/controllers/products.controller');
-const { allProductsModel, producByIdModel } = require('../../mocks/products.mock');
+const { productsController } = require('../../../src/controllers');
+const { allProductsModel, producByIdModel, newProduct, productResolvedError } = require('../../mocks/products.mock');
 
 describe('Realizando testes para listagem dos produtos no service', function () {
   it('Listando todos os produtos com sucesso no service', async function () {
     sinon.stub(productsModel, 'findAllModel').resolves(allProductsModel);
 
-    const findProducts = await products.getAllProducts();
+    const findProducts = await productsService.getAllProducts();
 
     expect(findProducts).to.be.an('array');
     expect(findProducts).to.be.deep.equal(allProductsModel);
@@ -20,7 +20,7 @@ describe('Realizando testes para listagem dos produtos no service', function () 
     sinon.stub(productsModel, 'findProductById').resolves([producByIdModel]);
 
     const productId = 1;
-    const findProductsById = await products.getProductById(productId);
+    const findProductsById = await productsService.getProductById(productId);
 
     expect(findProductsById).to.be.an('array');
     expect(findProductsById).to.be.deep.equal([producByIdModel]);
@@ -38,7 +38,90 @@ describe('Realizando testes para listagem dos produtos no service', function () 
     await productsController.getProductById(req, res);
 
     expect(res.status.calledWith(404));
-    expect(res.json.calledWith({ message: 'Product not found' }));
+    expect(res.json.calledWith(productResolvedError));
+  });
+
+  it('Cadastra produtos no service', async function () {
+    sinon.stub(productsModel, 'insertProductsModel').resolves(newProduct);
+    
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsService.insertProductsService(newProduct);
+
+    expect(res.status.calledWith(404));
+    expect(res.json.calledWith(productResolvedError));
+  });
+
+  it('Atualiza produtos no service', async function () {
+    const updateProductModelResolve = {
+      id: 1,
+      name: 'Martelo do Batman',
+    };
+    sinon.stub(productsModel, 'updateProductModel')
+      .resolves({ status: 200, data: updateProductModelResolve });
+    sinon.stub(productsModel, 'findProductById').resolves([updateProductModelResolve]);
+
+    const result = await productsService.updateProductService(updateProductModelResolve);
+
+    expect(result.data).to.be.equal(updateProductModelResolve);
+  });
+
+  it('Atualiza produtos no service com id inválido no service', async function () {  
+    sinon.stub(productsModel, 'findProductById').resolves([]);
+
+    const result = await productsService.updateProductService(123, 'Martelo do Batman');
+
+    expect(result.data).to.be.deep.equal({ message: 'Product not found' });
+    expect(result.status).to.be.equal(404);
+  });
+
+  it('Deleta um produto do service', async function () {
+    const deleteProductModelResolve = {
+      id: 1,
+      name: 'Martelo do Batman',
+    };
+    sinon.stub(productsModel, 'deleteProductModel').resolves({ status: 204 });
+    sinon.stub(productsModel, 'findProductById').resolves([deleteProductModelResolve]);
+
+    const result = await productsService.deleteProductService({ status: 204 });
+
+    expect(result.status).to.be.equal(204);
+  });
+
+  it('Deleta um produto do service com id inválido no service', async function () {
+    sinon.stub(productsModel, 'deleteProductModel').resolves(productResolvedError);
+    sinon.stub(productsModel, 'findProductById').resolves([]);
+
+    const result = await productsService.deleteProductService(22);
+
+    expect(result.data).to.be.deep.equal({ message: 'Product not found' });
+    expect(result.status).to.be.equal(404);
+  });
+
+  it('Pesquisa por um produto com sucesso no service', async function () {
+    const modelReturn = { status: 200, data: [{ id: 1, name: 'Martelo de Thor' }] };
+    sinon.stub(productsModel, 'searchProductModel').resolves(modelReturn);
+
+    const result = await productsService.searchProductsService('Martelo');
+
+    expect(result.data).to.be.deep.equal(modelReturn.data);
+    expect(result.status).to.be.equal(200);
+  });
+
+  it('Pesquisa todos os produtos sem o parametro "q" com sucesso no service', async function () {
+    const modelReturn = { status: 200, data: [{ id: 1, name: 'Martelo de Thor' }, { id: 2, name: 'Traje de encolhimento' }] };
+    const findAllModelReturn = [{ id: 1, name: 'Martelo de Thor' }, { id: 2, name: 'Traje de encolhimento' }];
+
+    sinon.stub(productsModel, 'searchProductModel').resolves(modelReturn);
+    sinon.stub(productsModel, 'findAllModel').resolves(findAllModelReturn);
+
+    const result = await productsService.searchProductsService();
+
+    expect(result.data).to.be.deep.equal(findAllModelReturn);
+    expect(result.status).to.be.equal(200);
   });
 
   afterEach(function () {
