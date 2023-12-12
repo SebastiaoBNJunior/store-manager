f272efec24ff4a1b8fec9f23b5e710ffefe65b7a
sebastiaobnj <junin_sbn@hotmail.com>
2023-12-12 12:29:14 -0300

    corrigindo lint 12-29

--- backend/src/middlewares/saleNewQuantity.validate.js
+++ backend/src/middlewares/saleNewQuantity.validate.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
   
   if (newQuantity.quantity === undefined) {
     return res.status(400).json({ message: '"quantity" is required' });
-    } 
+  } 
   if (newQuantity.quantity < 1) {
     return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
   }
